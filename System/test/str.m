710526d186dc2b5a8c4932406c13bc77e98cd908
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-03-31 19:49:10 +0900

    Use libdispatch(dummy version)

--- System/test/str.m
+++ System/test/str.m
@@ -1,13 +1,5 @@
 #import <Foundation/Foundation.h>
 
-#undef dispatch_once
-void dispatch_once(dispatch_once_t *predicate, dispatch_block_t block) {
-	if(*predicate != ~0l) {
-		*predicate = ~0l;
-		block();
-	}
-}
-
 int main(void)
 {
 	NSLog(@"Hello %@", @"world");

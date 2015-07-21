93a9215b609767d6cc2a0be51683da32e78793a6
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-07-21 17:12:30 +0900

    Add NSProgress new API

--- System/Foundation/include/Foundation/NSProgress.h
+++ System/Foundation/include/Foundation/NSProgress.h
@@ -1,11 +1,16 @@
 #import <Foundation/NSObject.h>
+#import <Foundation/NSDictionary.h>
 
 @interface NSProgress : NSObject
-
 + (id)currentProgress;
 + (id)progressWithTotalUnitCount:(int64_t)unitCount;
 
+- (NSProgress *)initWithParent:(NSProgress *)parentProgressOrNil userInfo:(NSDictionary *)userInfoOrNil;
+
 @property(readonly, getter=isCancelled) BOOL cancelled;
 @property int64_t completedUnitCount;
+@property int64_t totalUnitCount;
+@property(copy) void (^pausingHandler)(void);
+@property(copy) void (^cancellationHandler)(void);
 
 @end

b117b6dc3038a9b1162d56f0880a120050a128be
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-12-29 12:35:36 -0800

    Add mock methods for TestTransactionObserver

--- System/StoreKit/StoreKitTests/TestTransactionObserver.m
+++ System/StoreKit/StoreKitTests/TestTransactionObserver.m
@@ -1,4 +1,33 @@
 #import "TestTransactionObserver.h"
 
 @implementation TestTransactionObserver
+
+// Handing Transactions
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray/*<SKPaymentTransaction *>*/ *)transactions
+{
+    // FIXME: implement
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue removedTransactions:(NSArray/*<SKPaymentTransaction *>*/ *)transactions
+{
+    // FIXME: implement
+}
+
+// Handling Restored Transactions
+- (void)paymentQueue:(SKPaymentQueue *)queue restoreCompletedTransactionsFailedWithError:(NSError *)error
+{
+    // FIXME: implement
+}
+
+- (void)paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
+{
+    // FIXME: implement
+}
+
+// Handling Download Actions
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedDownloads:(NSArray/*<SKDownload *>*/ *)downloads
+{
+    // FIXME: implement
+}
+
 @end

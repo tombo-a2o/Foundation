bb10886f4c79790824a86b56432acfbf8b4b8327
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-01-22 17:31:40 -0800

    Add root view

--- System/StoreKit/StoreKitServer/config/routes.rb
+++ System/StoreKit/StoreKitServer/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: 'root#index'
   resources :products, defaults: { format: :json }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

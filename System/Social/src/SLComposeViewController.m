83eca60d10580310260395c3cb9df9e3dfe26320
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-03-09 15:03:39 +0900

    Twitter view

--- System/Social/src/SLComposeViewController.m
+++ System/Social/src/SLComposeViewController.m
@@ -1,4 +1,5 @@
 #import <Social/SLComposeViewController.h>
+#import <UIKit/UIKit.h>
 
 NSString *const SLServiceTypeFacebook = @"facebook";
 NSString *const SLServiceTypeTwitter = @"twitter";
@@ -9,13 +10,77 @@ NSString *const SLServiceTypeTencentWeibo = @"tencentWeibo";
 
 + (SLComposeViewController *)composeViewControllerForServiceType:(NSString *)serviceType
 {
-    SLComposeViewController *controller = [[SLComposeViewController alloc] init];
-    return controller;
+    return [[SLComposeViewController alloc] initForServiceType:serviceType];
 }
 
 + (BOOL)isAvailableForServiceType:(NSString *)serviceType
 {
     return [serviceType isEqualToString:SLServiceTypeFacebook] || [serviceType isEqualToString:SLServiceTypeTwitter];
+}
+
+- (instancetype)initForServiceType:(NSString *)serviceType
+{
+    self = [super init];
+    if(self) {
+        // self.modalPresentationStyle = UIModalPresentationOverCurrentContext;
+        self.modalPresentationStyle = UIModalPresentationCurrentContext;
+        self.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+        _serviceType = serviceType;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    if([_serviceType isEqualToString:SLServiceTypeTwitter]) {
+        [self _prepareTweetView];
+    } else if([_serviceType isEqualToString:SLServiceTypeFacebook]) {
+        
+    } else {
+        assert(0);
+    }
+    
+}
+
+- (void)_prepareTweetView
+{
+    UIView *view = self.view;
+    view.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.3];
+    
+    UIView *rootView = [[UIView alloc] initWithFrame:CGRectMake(30,115,260,260)];
+    rootView.backgroundColor = [UIColor whiteColor];
+    rootView.layer.cornerRadius = 10;
+    [view addSubview:rootView];
+
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 240, 40)];
+    titleLabel.text = @"Twitter";
+    titleLabel.textColor = [UIColor blackColor];
+    titleLabel.font = [UIFont boldSystemFontOfSize:17];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.lineBreakMode = NSLineBreakByCharWrapping;
+    titleLabel.numberOfLines = 0;
+//    titleLabel.backgroundColor = [UIColor redColor];
+    [rootView addSubview:titleLabel];
+    
+    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(180, 0, 80, 40)];
+    [closeButton setTitleColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0] forState:UIControlStateNormal];
+    closeButton.titleLabel.font = [UIFont boldSystemFontOfSize:13];
+    closeButton.titleLabel.textAlignment = NSTextAlignmentRight;
+    [closeButton setTitle:@"閉じる" forState:UIControlStateNormal];
+    [closeButton addTarget:self action:@selector(closeWindow:) forControlEvents:UIControlEventTouchUpInside];
+//    closeButton.backgroundColor = [UIColor blueColor];
+//    closeButton.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    [rootView addSubview:closeButton];
+    
+    UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(titleLabel.frame)+5, 240, 200)];
+    [webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://twitter.com/intent/tweet"]]];
+    [rootView addSubview:webview];
+}
+
+- (void)closeWindow:(UIButton*)button
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (BOOL)setInitialText:(NSString *)text

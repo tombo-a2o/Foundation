18b6cd4d6bcde58b1b64f764fa9ba01a010db728
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-04-18 16:09:45 -0700

    Fix for Native iOS

--- System/StoreKit/src/AFNetworking-3.0.4/SKAFNetworking/SKAFNetworkEmscripten.h
+++ System/StoreKit/src/AFNetworking-3.0.4/SKAFNetworking/SKAFNetworkEmscripten.h
@@ -1,11 +1,19 @@
 // FIX_FOR_EMSCRIPTEN:
 
 #ifndef _SKAFNETWORK_EMSCRIPTEN_
+#define _SKAFNETWORK_EMSCRIPTEN_
 
+// Define nullable keywords
+#ifndef NS_ASSUME_NONNULL_BEGIN
 #define NS_ASSUME_NONNULL_BEGIN
 #define nullable
 #define _Nullable
 #define NS_ASSUME_NONNULL_END
+#endif
+
+// Define NS_SWIFT_NOTHROW
+#ifndef NS_SWIFT_NOTHROW
 #define NS_SWIFT_NOTHROW
+#endif
 
 #endif

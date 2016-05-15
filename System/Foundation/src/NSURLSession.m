3d6f35960449d773a6ab1500a5d7ac8cfc41f024
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-05-16 01:40:27 +0900

    Add dummy implementation

--- System/Foundation/src/NSURLSession.m
+++ System/Foundation/src/NSURLSession.m
@@ -1,8 +1,16 @@
 #import <Foundation/NSURLSession.h>
 
 @implementation NSURLSession
++ (NSURLSession *)sessionWithConfiguration:(NSURLSessionConfiguration *)configuration
+{
+    return nil;
+}
 @end
 @implementation NSURLSessionConfiguration
++ (NSURLSessionConfiguration *)ephemeralSessionConfiguration
+{
+    return nil;
+}
 @end
 @implementation NSURLSessionTask
 @end

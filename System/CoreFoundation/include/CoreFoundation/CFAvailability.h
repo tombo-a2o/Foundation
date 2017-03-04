268e60a3654ee27c06841f2bd82c944fc6c2685a
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2017-03-03 22:39:09 -0800

    Change CF_ENUM into the latest definition

--- System/CoreFoundation/include/CoreFoundation/CFAvailability.h
+++ System/CoreFoundation/include/CoreFoundation/CFAvailability.h
@@ -182,16 +182,33 @@
 #endif
 
 // Enums and Options
+#define __CF_ENUM_GET_MACRO(_1, _2, NAME, ...) NAME
 #if (__cplusplus && __cplusplus >= 201103L && (__has_extension(cxx_strong_enums) || __has_feature(objc_fixed_enum))) || (!__cplusplus && __has_feature(objc_fixed_enum))
-#define CF_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
+#define __CF_NAMED_ENUM(_type, _name)     enum _name : _type _name; enum _name : _type
+#define __CF_ANON_ENUM(_type)             enum : _type
 #if (__cplusplus)
 #define CF_OPTIONS(_type, _name) _type _name; enum : _type
 #else
 #define CF_OPTIONS(_type, _name) enum _name : _type _name; enum _name : _type
 #endif
 #else
-#define CF_ENUM(_type, _name) _type _name; enum
+#define __CF_NAMED_ENUM(_type, _name) _type _name; enum
+#define __CF_ANON_ENUM(_type) enum
 #define CF_OPTIONS(_type, _name) _type _name; enum
 #endif
+
+/* CF_ENUM supports the use of one or two arguments. The first argument is always the integer type used for the values of the enum. The second argument is an optional type name for the macro. When specifying a type name, you must precede the macro with 'typedef' like so:
+
+typedef CF_ENUM(CFIndex, CFComparisonResult) {
+    ...
+};
+
+If you do not specify a type name, do not use 'typdef', like so:
+
+CF_ENUM(CFIndex) {
+    ...
+};
+*/
+#define CF_ENUM(...) __CF_ENUM_GET_MACRO(__VA_ARGS__, __CF_NAMED_ENUM, __CF_ANON_ENUM)(__VA_ARGS__)
 
 #endif // __COREFOUNDATION_CFAVAILABILITY__

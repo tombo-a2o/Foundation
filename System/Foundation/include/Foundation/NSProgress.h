5c2f39fa1c9eb21239c16e31197139a82a7afa27
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-07-24 18:58:31 +0900

    Implement simplest NSProgress methods

--- System/Foundation/include/Foundation/NSProgress.h
+++ System/Foundation/include/Foundation/NSProgress.h
@@ -1,19 +1,48 @@
 #import <Foundation/NSObject.h>
 #import <Foundation/NSDictionary.h>
 
+typedef NSString *NSProgressKind;
+typedef NSString *NSProgressFileOperationKind;
+typedef NSString *NSProgressUserInfoKey;
+
 @interface NSProgress : NSObject
+- (instancetype)initWithParent:(NSProgress *)parentProgressOrNil
+                      userInfo:(NSDictionary<NSProgressUserInfoKey, id> *)userInfoOrNil;
++ (NSProgress *)discreteProgressWithTotalUnitCount:(int64_t)unitCount;
++ (NSProgress *)progressWithTotalUnitCount:(int64_t)unitCount;
++ (NSProgress *)progressWithTotalUnitCount:(int64_t)unitCount
+                                    parent:(NSProgress *)parent
+                          pendingUnitCount:(int64_t)portionOfParentTotalUnitCount;
+
 + (id)currentProgress;
-+ (id)progressWithTotalUnitCount:(int64_t)unitCount;
+- (void)becomeCurrentWithPendingUnitCount:(int64_t)unitCount;
+- (void)addChild:(NSProgress *)child
+withPendingUnitCount:(int64_t)inUnitCount;
+- (void)resignCurrent;
 
-- (NSProgress *)initWithParent:(NSProgress *)parentProgressOrNil userInfo:(NSDictionary *)userInfoOrNil;
-
-@property(readonly, getter=isCancelled) BOOL cancelled;
-@property int64_t completedUnitCount;
 @property int64_t totalUnitCount;
-@property(copy) void (^pausingHandler)(void);
-@property(copy) void (^cancellationHandler)(void);
+@property int64_t completedUnitCount;
+@property(copy) NSString *localizedDescription;
+@property(copy) NSString *localizedAdditionalDescription;
+
+@property(readonly) double fractionCompleted;
+
 @property(getter=isCancellable) BOOL cancellable;
+@property(readonly, getter=isCancelled) BOOL cancelled;
+- (void)cancel;
+@property(copy) void (^cancellationHandler)(void);
 @property(getter=isPausable) BOOL pausable;
+@property(readonly, getter=isPaused) BOOL paused;
+- (void)pause;
+@property(copy) void (^pausingHandler)(void);
+- (void)resume;
 @property(copy) void (^resumingHandler)(void);
+
+@property(readonly, getter=isIndeterminate) BOOL indeterminate;
+@property(copy) NSProgressKind kind;
+- (void)setUserInfoObject:(id)objectOrNil
+                   forKey:(NSProgressUserInfoKey)key;
+@property(readonly, copy) NSDictionary<NSProgressUserInfoKey, id> *userInfo;
+
 
 @end
--- System/Foundation/src/NSProgress.m
+++ System/Foundation/src/NSProgress.m
@@ -1,36 +1,111 @@
-//
-//  NSProgress.m
-//  Foundation
-//
-//  Copyright (c) 2014 Apportable. All rights reserved.
-//
+#import <Foundation/Foundation.h>
 
-#import <Foundation/NSProgress.h>
+@implementation NSProgress {
+    int64_t _totalUnitCount;
+    int64_t _completedUnitCount;
+}
 
-@implementation NSProgress
+- (instancetype)initWithParent:(NSProgress *)parentProgressOrNil userInfo:(NSDictionary<NSProgressUserInfoKey, id> *)userInfoOrNil
+{
+    if(parentProgressOrNil) {
+        NSLog(@"*** %s: Hierarchical NSProgress is not supported", __FUNCTION__);
+        return nil;
+    }
+    if(userInfoOrNil) {
+        NSLog(@"*** %s: userInfo is not supported", __FUNCTION__);
+        return nil;
+    }
+
+    return self;
+}
+
++ (NSProgress *)discreteProgressWithTotalUnitCount:(int64_t)unitCount
+{
+    NSProgress *progress = [[NSProgress alloc] initWithParent:nil userInfo:nil];
+    progress.totalUnitCount = unitCount;
+    return progress;
+}
+
++ (NSProgress *)progressWithTotalUnitCount:(int64_t)unitCount
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+
++ (NSProgress *)progressWithTotalUnitCount:(int64_t)unitCount parent:(NSProgress *)parent pendingUnitCount:(int64_t)portionOfParentTotalUnitCount
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
 
 + (id)currentProgress
 {
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
     return nil;
 }
 
-+ (id)progressWithTotalUnitCount:(int64_t)unitCount
+- (void)becomeCurrentWithPendingUnitCount:(int64_t)unitCount
 {
-    return nil;
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
 }
 
-- (BOOL)isCancelled
+- (void)addChild:(NSProgress *)child withPendingUnitCount:(int64_t)inUnitCount
 {
-    return NO;
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
 }
 
-- (void)setCompletedUnitCount:(int64_t)count
+- (void)resignCurrent
 {
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)cancel
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)pause
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)resume
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)setUserInfoObject:(id)objectOrNil forKey:(NSProgressUserInfoKey)key
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)setTotalUnitCount:(int64_t)totalUnitCount
+{
+    [self willChangeValueForKey:@"fractionCompleted"];
+    _totalUnitCount = totalUnitCount;
+    [self didChangeValueForKey:@"fractionCompleted"];
+}
+
+- (int64_t)totalUnitCount
+{
+    return _totalUnitCount;
+}
+
+- (void)setCompletedUnitCount:(int64_t)completedUnitCount
+{
+    [self willChangeValueForKey:@"fractionCompleted"];
+    _completedUnitCount = completedUnitCount;
+    [self didChangeValueForKey:@"fractionCompleted"];
 }
 
 - (int64_t)completedUnitCount
 {
-    return 0;
+    return _completedUnitCount;
+}
+
+- (double)fractionCompleted
+{
+    return 1.0f * _totalUnitCount / _completedUnitCount;
 }
 
 @end
